72a823278c68c8c6dae660a35a8ce8bc708523dc
João Victor <joaovictordev0820@gmail.com>
2025-08-27 11:46:08 -0300

    added header frontend

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,15 +1,34 @@
 import logoImg from "../assets/img/bgImage.png"
+import avatarImg from "../assets/img/avatarImage.jpg"
 
 export const Header = () => {
     return (
         <header className="h-[80px] flex items-center justify-between">
-            <div className="container flex justify-between items-center px-2 mx-auto">
-                <div className="flex items-center justify-center">
+            <div className="container flex justify-between items-center px-2 mx-auto lg:justify-normal">
+                <div className="flex items-center justify-start w-[33%] h-[80px]">
                     <div className="w-10 h-10 rounded-full mt-1 mr-1"><img src={logoImg.src} /></div>
                     <h1 className="text-xl font-bold">Streaming Tracker</h1>
                 </div>
-                <div>
+                <div className="w-[33%] h-[80px] flex items-center justify-center">
+                    {/* Mobile menu */}
                     <button className="rotate-90 cursor-pointer font-bold text-xl lg:hidden">|||</button>
+
+                    {/* Desktop menu */}
+                    <nav>
+                        <ul className="list-none flex justify-between w-[300px]">
+                            <a href=""><li className="border-b-2 border-transparent text-lg hover:border-primary">Home</li></a>
+                            <a href=""><li className="border-b-2 border-transparent text-lg hover:border-primary">News</li></a>
+                            <a href=""><li className="border-b-2 border-transparent text-lg hover:border-primary">Trendings</li></a>
+                            <a href=""><li className="border-b-2 border-transparent text-lg hover:border-primary">My lists</li></a>
+                        </ul>
+                    </nav>
+                </div>
+                <div className="w-[33%] h-[80px] flex items-center justify-end">
+                    <div className="w-15 h-15 rounded-full border-2">
+                        <img 
+                            className="w-full h-auto rounded-full cursor-pointer"
+                            src={avatarImg.src}/>
+                    </div>
                 </div>
             </div>
         </header>
--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -11,16 +11,19 @@ export const Login = () => {
         <section className="w-screen h-screen text-primary">
             <Header />
             <div className="container mx-auto h-[calc(100vh-80px)] flex justify-center">
+                {/* Info side */}
                 <div className="hidden w-[50%] lg:flex">
                     <div className="w-full">
                         <img src={bgImage.src}/>
                     </div>
                 </div>
+
+                {/* Form side */}
                 {!toggleForm &&
                     <div className="flex flex-col w-[50%]">
                         <div className="flex items-center justify-center h-[80px] mt-[50px]">
-                            <button onClick={() => setToggleForm(false)} className="bg-primary text-white rounded-l-lg w-[100px] h-[50px]">Sign in</button>
-                            <button onClick={() => setToggleForm(true)} className="border-2 rounded-r-lg w-[100px] h-[50px]">Sign up</button>
+                            <button onClick={() => setToggleForm(false)} className="bg-primary text-white rounded-l-lg w-[100px] h-[50px] cursor-pointer hover:bg-primary/90">Sign in</button>
+                            <button onClick={() => setToggleForm(true)} className="border-2 rounded-r-lg w-[100px] h-[50px] cursor-pointer hover:bg-gray-100">Sign up</button>
                         </div>
                         <div className="flex flex-col items-center h-[500px]">
                             <label className="mt-[50px] flex flex-col text-lg">
@@ -35,16 +38,16 @@ export const Login = () => {
                                     className="border border-primary rounded-lg px-3 py-2 mt-1 w-[250px] outline-0"
                                     type="password" name="" id="" placeholder="Type your Username" />
                             </label>
-                            <button className="bg-primary text-white rounded-lg mt-[25px] w-[100px] h-[50px]">Login</button>
+                            <button className="bg-primary text-white rounded-lg mt-[25px] w-[100px] h-[50px] cursor-pointer hover:bg-primary/90">Login</button>
                         </div>
                     </div>
                 }
 
                 {toggleForm &&
-                    <div className="flex flex-col">
+                    <div className="flex flex-col w-[50%]">
                         <div className="flex items-center justify-center h-[80px] mt-[50px]">
-                            <button onClick={() => setToggleForm(false)} className="border-2 rounded-l-lg w-[100px] h-[50px]">Sign in</button>
-                            <button onClick={() => setToggleForm(true)} className="bg-primary text-white rounded-r-lg w-[100px] h-[50px]">Sign up</button>
+                            <button onClick={() => setToggleForm(false)} className="border-2 rounded-l-lg w-[100px] h-[50px] cursor-pointer hover:bg-gray-100">Sign in</button>
+                            <button onClick={() => setToggleForm(true)} className="bg-primary text-white rounded-r-lg w-[100px] h-[50px] cursor-pointer hover:bg-primary/90">Sign up</button>
                         </div>
                         <div className="flex flex-col items-center h-[500px]">
                             <label className="mt-[50px] flex flex-col text-lg">
@@ -65,7 +68,7 @@ export const Login = () => {
                                     className="border border-primary rounded-lg px-3 py-2 mt-1 w-[250px] outline-0"
                                     type="password" name="" id="" placeholder="Type your Username" />
                             </label>
-                            <button className="bg-primary text-white rounded-lg mt-[25px] w-[100px] h-[50px]">Login</button>
+                            <button className="bg-primary text-white rounded-lg mt-[25px] w-[100px] h-[50px] cursor-pointer hover:bg-primary/90">Login</button>
                         </div>
                     </div>
                 }
