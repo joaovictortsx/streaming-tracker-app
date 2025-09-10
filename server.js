bf098627076410633ebbc064dbb318d8bed13eb7
João Victor <joaovictordev0820@gmail.com>
2025-09-10 17:58:46 -0300

    feat: v1 API  favorites, watched, seeLater

--- server.js
+++ server.js
@@ -1,6 +1,107 @@
 import express from "express";
+import { PrismaClient } from './src/generated/prisma/index.js'
 
-/* Express configs */
+
+// Prisma 
+const prisma = new PrismaClient();
+
+// Express init and configs
 const app = express();
 app.use(express.json());
 app.listen(3000)
+
+
+// Rota para adicionar novo item a lista
+app.post('/add', async (req, res) => {
+    await prisma.list.create({
+        data: {
+            mal_id: req.body.malId,
+            title: req.body.title,
+            image: req.body.image,
+            favorite: req.body.favorite,
+            watched: req.body.watched,
+            see_later: req.body.seeLater
+        }
+    });
+
+    res.status(201).json(req.body);
+})
+
+
+// Inicio Rotas "Favorites" ----------------------------
+// Rota para pegar favoritos da lista
+app.get('/favorites', async (req, res) => {
+    const favorites = await prisma.list.findMany({
+        where: {
+            favorite: true
+        }
+    });
+    res.status(201).json(favorites);
+})
+
+
+// Rota para editar favorito da lista
+app.put('/favorite/:id', async (req, res) => {
+    await prisma.list.update({
+        where: {
+            mal_id: req.body.malId
+        },
+        data: {
+            favorite: req.body.favorite
+        }
+    });
+
+    res.status(200).json(req.body);
+})
+
+// Fim Rotas "Favorite" ---------------------------------------
+// Inicio Rotas "Watched" -------------------------------
+// Rota para pegar marcados como assistido da lista
+app.get('/watcheds', async (req, res) => {
+    const watcheds = await prisma.list.findMany({
+        where: {
+            watched: true
+        }
+    });
+    res.status(201).json(watcheds);
+})
+
+
+// Rota para editar assistidos da lista
+app.put('/watched/:id', async (req, res) => {
+    await prisma.list.update({
+        where: {
+            mal_id: req.body.malId
+        },
+        data: {
+            watched: req.body.watched
+        }
+    });
+    res.status(200).json(req.body);
+})
+
+//Fim Rotas "Watched" -------------------------------
+//Inicio Rotas "See later" --------------------------
+//Rota para pegar Ver depois na lista
+app.get('/seeLater', async (req, res) => {
+    const seeLater = await prisma.list.findMany({
+        where: {
+            see_later: true
+        }
+    });
+    res.status(201).json(seeLater);
+})
+
+
+// Rota para editar ver depois na lista
+app.put('/seeLater/:id', async (req, res) => {
+    await prisma.list.update({
+        where: {
+            mal_id: req.body.malId
+        },
+        data: {
+            see_later: req.body.seeLater
+        }
+    })
+})
+//Fim Rotas "See later" --------------------
