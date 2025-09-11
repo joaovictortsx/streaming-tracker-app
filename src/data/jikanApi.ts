fe0319664bcb54c55f013154bd546570b0b33fa3
João Victor <joaovictordev0820@gmail.com>
2025-09-11 12:14:12 -0300

    fix: tweaks on api

--- src/data/jikanApi.ts
+++ src/data/jikanApi.ts
@@ -8,28 +8,28 @@ const url = axios.create({
 
 
 /* Request da lista de animes da nova temporada */
-export const getNewSeasonAnimes = async (): Promise<Anime[] | any> => {
-    const res = await url.get('/seasons/now?filter=tv');
+export const getNewSeasonAnimes = async (): Promise<Anime[]> => {
+    const res = await url.get<Anime[]>('/seasons/now?filter=tv');
     return res.data;
 }
 
 
 /* Request da lista de animes que estão melhores avaliados da nova temporada */
-export const getTrendingAnimes = async (): Promise<Anime[] | any> => {
-    const res = await url.get('/top/anime?type=tv&filter=airing');
+export const getTrendingAnimes = async (): Promise<Anime[]> => {
+    const res = await url.get<Anime[]>('/top/anime?type=tv&filter=airing');
     return res.data;
 }
 
 
 /* Request de apenas 1 anime usando id como filtro */
-export const getSingleAnime = async (id: string): Promise<Anime | any> => {
-    const res = await url.get(`/anime/${id}`);
+export const getSingleAnime = async (id: string): Promise<Anime> => {
+    const res = await url.get<Anime>(`/anime/${id}`);
     return res.data;
 }
 
 
 /* Request search */
-export const getSearchAnime = async (name: string): Promise<Anime | any> => {
-    const res = await url.get(`/anime?type=tv&q=${name}`);
+export const getSearchAnime = async (name: string): Promise<Anime> => {
+    const res = await url.get<Anime>(`/anime?type=tv&q=${name}`);
     return res.data;
 }
